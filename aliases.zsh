8bf42d9da534c2b1373b4d4c1345270e1c0d9bbc
David Feng <fenghourun@gmail.com>
2024-05-22 02:46:55 -0400

    new aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,7 @@
 # Aliases
 
 alias v="nvim"
+alias g="git"
 alias aliases="v ~/.config/zsh/aliases.zsh"
 
 # Open config
@@ -9,6 +10,7 @@ alias zshrc="v ~/.config/zsh/.zshrc"
 alias starshiprc="v ~/.config/starship/starship.toml"
 alias vimrc="v ~/.config/nvim/init.vim"
 alias neofetchrc="v ~/.config/neofetch/config.conf"
+alias fdignore="v ~/.config/fd/ignore/.fdignore"
 
 # Goto config
 alias vimdir="cd ~/.config/nvim"
@@ -18,6 +20,9 @@ alias neofetchdir="cd ~/.config/neofetch"
 
 alias docs="cd ~/Documents"
 alias be="cd ~/Documents/shake-hosted-wallet"
+alias fe="cd ~/Documents/shaketh"
+alias web="cd ~/Documents/shake-web"
+alias sb="cd ~/Documents/shakeboard"
 
 benchmark () {
    ts '[%Y-%m-%d %H:%M:%S]'
